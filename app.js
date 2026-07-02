8cda3be080db3330a49b9e92d1fafe2e67a41586
Muhammad hashim <mh4165209@gmail.com>
2026-07-02 14:37:32 +0500

    question 02 complete

--- app.js
+++ app.js
@@ -5,4 +5,33 @@ const price=999.99;
 const inStock=true;
 console.log(productName,price,inStock);
 
+// Question 2: Mathematical Operations
+// Calculate and display:
+
+// The remainder when 27 is divided by 4
+// The square of 12 (using exponentiation operator **)
+// The result of incrementing 8 by 1 using the ++ operator
+// The result of decrementing 15 by 2 using the -= operator
+
+let value01=27;
+let value02=4;
+let valuesDevide=27 / 4;
+let valuesRemainder=27 % 4;
+
+let value03=12;
+let value03Square=12**12;
+
+let value04=8;
+++value04;
+
+let value05=15;
+value05 -= 2;
+console.log(value05);
+
+
+
+
+
+
+
 
