c060012e6d87c4404a2804ccef4ee6d75a85e660
Muhammad hashim <mh4165209@gmail.com>
2026-07-02 13:30:35 +0500

    question 1 complete

---
+++ app.js
@@ -0,0 +1,8 @@
+// Question 1: Variable Declarations and Initialization
+// Create three variables: productName (string with value "Laptop"), price (number with value 999.99), and inStock (boolean with value true). Console.log all three variables in a single statement.
+const productName='laptop';
+const price=999.99;
+const inStock=true;
+console.log(productName,price,inStock);
+
+
